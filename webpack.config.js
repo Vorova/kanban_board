0d0ffd7ec8b26e6ca61185a7f689238d141f962e
vorova <vvvorova@gmail.com>
2024-02-18 21:15:23 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'html', 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+          filename: 'index.[contenthash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader',
+                ],
+            }
+        ]
+    },
+};
